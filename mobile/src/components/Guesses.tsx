822afb49adf7774b32541922b9b1fa92909c848f
Vinícius Santos <vinisantoosk@gmail.com>
2022-11-07 02:51:07 -0300

    Inserção do projeto Mobile

---
+++ mobile/src/components/Guesses.tsx
@@ -0,0 +1,101 @@
+import { useToast, FlatList } from 'native-base'
+import { useState, useEffect } from 'react'
+
+import { api } from '../services/api'
+
+import { EmptyMyPoolList } from '../components/EmptyMyPoolList'
+import { Game, GameProps } from '../components/Game'
+import { Loading } from './Loading'
+
+interface Props {
+  poolId: string;
+  code: string;
+}
+
+export function Guesses({ poolId, code }: Props) {
+  const [isLoading, setIsLoading] = useState(true)
+  const [games, setGames] = useState<GameProps[]>([])
+  const [firstTeamPoints, setFirstTeamPoints] = useState('')
+  const [secondTeamPoints, setSecondTeamPoints] = useState('')
+
+  const toast = useToast()
+
+  async function fetchGames() {
+    try {
+      setIsLoading(true)
+      const response = await api.get(`/pools/${poolId}/games`)
+      setGames(response.data.games)
+    } catch (error) {
+      console.log(error)
+
+      toast.show({
+        title: 'Não foi possível carregar as informações do bolão',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  async function handleGuessConfirm(gameId: string) {
+    try {
+      if (!firstTeamPoints.trim() || !secondTeamPoints.trim()) {
+        return toast.show({
+          title: 'Informe o placar do palpite.',
+          placement: 'top',
+          bgColor: 'red.500'
+        })
+      }
+
+      await api.post(`/pools/${poolId}/games/${gameId}/guesses`, {
+        firstTeamPoints: Number(firstTeamPoints),
+        SecondTeamPoints: Number(secondTeamPoints)
+      })
+
+      toast.show({
+        title: 'Palpite enviado com sucesso.',
+        placement: 'top',
+        bgColor: 'green.500'
+      })
+
+      fetchGames()
+    } catch (error) {
+      console.log(error)
+
+      toast.show({
+        title: 'Não foi possível enviar o palpite.',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchGames()
+  }, [poolId])
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  return (
+    <FlatList
+      data={games}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => (
+        
+        <Game
+          data={item}
+          setFirstTeamPoints={setFirstTeamPoints}
+          setSecondTeamPoints={setSecondTeamPoints}
+          onGuessConfirm={() => handleGuessConfirm(item.id)}
+        />
+      )}
+      _contentContainerStyle={{pb:10}}
+      ListEmptyComponent={() => <EmptyMyPoolList code={code}/>}
+    />
+  )
+}
